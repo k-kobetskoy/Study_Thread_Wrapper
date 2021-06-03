b6ceb1b34aecdc3c8ed0b19ba177adb1f38cb109
Konstantin Kobetskoy <k.kobetskoy@outlook.com>
2021-06-03 16:54:21 +0300

    Add project files.

---
+++ Experiments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
+
+namespace Experiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to begin");
+            Console.ReadKey();
+            var tw = new ThreadWorker<int>(Count);
+
+            var result = tw.Start(200);
+            
+            Console.WriteLine($"result: {result}");
+            
+            Console.WriteLine($"Ошибки: {tw.Exception}\nУспех: {tw.IsSuccess}\nПровал:{tw.IsFaulted}");
+        }
+
+        public static int Count(object n)
+        {
+            var num = (int) n;
+            var result = 0;
+            for (var i = 0; i < num; i++)
+            {
+                result += i;
+                Console.Write("-");
+                Thread.Sleep(10);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Experiments/ThreadWorker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Experiments
+{
+    class ThreadWorker<Tresult>
+    {
+        private readonly Func<object, Tresult> func;
+        private object param;
+        private Tresult result=default;
+
+        public Tresult Result
+        {
+            get{
+                while (!IsCompleted)
+                {
+                    Console.WriteLine("Wait");
+                    Thread.Sleep(150);
+                }
+                return result;
+            }
+            private set => result = value;
+        }
+
+        public bool IsCompleted { get; private set; } = false;
+        public bool IsSuccess { get; private set; } = false;
+        public bool IsFaulted { get; private set; } = false;
+        public Exception Exception { get; private set; } = null;
+
+        public ThreadWorker(Func<object, Tresult> func)
+        {
+            this.func = func ?? throw new ArgumentNullException(nameof(func));
+        }
+
+        public Tresult Start(object methodParam)
+        {
+            this.param = methodParam;
+            var thread = new Thread(ThreadExec);
+            thread.Start();
+            return Result;
+        }
+
+        public void ThreadExec()
+        {
+            try
+            {
+                result = func.Invoke(param);
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                IsFaulted = true;
+            }
+            finally
+            {
+                IsSuccess = Exception is null;
+                IsCompleted = true;
+            }
+        }
+
+        public void Wait()
+        {
+            while (IsCompleted == false)
+            {
+                Console.WriteLine("Wait");
+                Thread.Sleep(150);
+            }
+        }
+
+    }
+}
